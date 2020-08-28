9883b4a39d650c2ee995a3aed24123df3d61eb5f
joaquim <joaquim@localhost.mycentos7>
2020-08-27 23:57:49 -0300

    testJson1

---
+++ testJson1.go
@@ -0,0 +1,59 @@
+package main
+import "fmt"
+import "os"
+import "io/ioutil"
+import "encoding/json"
+import "strconv"
+
+// Users struct which contains an array of users
+type Users struct {
+    Users []User `json:"users"`
+}
+
+// User struct which contains a name a type and a list of social links
+type User struct {
+    Name   string `json:"name"`
+    Type   string `json:"type"`
+    Age    int    `json:"Age"`
+    Social Social `json:"social"`
+}
+
+// Social struct which contains a list of links
+type Social struct {
+    Facebook string `json:"facebook"`
+    Twitter  string `json:"twitter"`
+}
+
+func main() {
+    fmt.Println("hello world")
+
+    // Open our jsonFile
+    jsonFile, err := os.Open("json1.json")
+    // if we os.Open returns an error then handle it
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println("Successfully Opened users.json")
+    // defer the closing of our jsonFile so that we can parse it later on
+    defer jsonFile.Close()
+
+    // read our opened jsonFile as a byte array.
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+
+    // we initialize our Users array
+    var users Users
+
+    // we unmarshal our byteArray which contains our jsonFile's content into 'users' which we defined above
+    json.Unmarshal(byteValue, &users)
+
+    // we iterate through every user within our users array and
+    // print out the user Type, their name, and their facebook url
+    // as just an example
+    for i := 0; i < len(users.Users); i++ {
+        fmt.Println()
+        fmt.Println("User Type: " + users.Users[i].Type)
+        fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
+        fmt.Println("User Name: " + users.Users[i].Name)
+        fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
+    }    
+}
